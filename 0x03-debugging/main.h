a8ebf2a29e4a65f534744cca395779473c1b7526
Sinofipaza <fipazasino@gmail.com>
2023-02-22 12:14:52 -0800

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,4 +8,24 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif /* main.h  */
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+{
+int a, b, c;
+int largest;
+
+a = 972;
+b = -98
+c = 0;
+
+largest = largest_number(a, b, c);
+
+printf("%d is the largest number\n", largest);
+
+return (0);
+}
+#endif /** main_h */
