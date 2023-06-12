623636b99b0a6ef77a1123cb113ea311c2393430
Sinofipaza <fipazasino@gmail.com>
2023-06-12 05:13:24 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - a program that multiplies two numbers.
- * Return: 0
+ * main - function to multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: an array containing command line arguments
+ * Return: 0 if it runs
  */
 
-int main(void)
-
+int main(int argc, char *argv[])
 {
+	int x, y;
 
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-	i = 0;
-
-	printf("Infinite loop avoided! \\o/\n");
+	if (argc != 3)
+		printf("Error\n");
+	else
+	{
+		x = strtol(argv[1], 0, 10);
+		y = strtol(argv[2], 0, 10);
+		printf("%d\n", x * y);
+	}
 	return (0);
 }
