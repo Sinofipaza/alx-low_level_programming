490ff81e73e503419f6f6777c401b83474429c4b
Sinofipaza <fipazasino@gmail.com>
2023-06-21 13:37:38 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - counter
@@ -10,22 +11,18 @@
 
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
+	if (get_op_func(argv[2]))
+		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	else
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
