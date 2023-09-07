8975b18fddc75b6c8d29eb69b63ce59987dfb373
Sinofipaza <fipazasino@gmail.com>
2023-09-07 09:47:59 -0700

    python

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+  * key_index - gives you index of key
+  * @key: key
+  * @size: size of array of hash table
+  * Return: index of key
+  */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
